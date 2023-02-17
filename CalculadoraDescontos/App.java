2db1365bf3c2a5caa0596caf4d601a55af9b7ef5
AdonaiBatista <adonaibatista20@gmail.com>
2023-02-17 19:21:15 -0300

    fgggdf

--- CalculadoraDescontos/App.java
+++ CalculadoraDescontos/App.java
@@ -1,10 +1,15 @@
 package CalculadoraDescontos;
+
 /**
  * App
  */
 public class App {
 
     public static void main(String[] args) {
-        
+        // CRIAR O MEU OBJETO DA MINHA CLASSE CALCULADORA DESCONTO
+        calculadoraDesconto objeto1 = new calculadoraDesconto();
+        objeto1.calcular20();
+        objeto1.calcular30();
+
     }
 }
--- CalculadoraDescontos/calculadoraDesconto.java
+++ CalculadoraDescontos/calculadoraDesconto.java
@@ -1,1 +1,29 @@
 package CalculadoraDescontos;
+
+import java.util.Scanner;
+
+import javax.security.auth.x500.X500Principal;
+
+//CRIAR UMA CLASSE
+public class calculadoraDesconto {
+    Scanner sc = new Scanner(System.in);
+
+    // DECLARAR OBJETOS
+    public void calcular20() {
+        System.out.println("Informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        final int desconto20 = 20;
+        double precoComDesconto = precoProduto - (precoProduto * desconto20 / 100);
+        System.out.println("O Preço com Desconto é " + precoComDesconto + "reais");
+    
+    }
+
+    // CRIAR MÉTODOS
+    public void calcular30() {
+        System.out.println("Informe o Preço do Produto");
+        double precoProduto = sc.nextDouble();
+        final int desconto30 = 30;
+        double precoComDesconto = precoProduto - (precoProduto * desconto30 / 100);
+        System.out.println("O Preço com Desconto é " + precoComDesconto + "reais");
+    }
+}
